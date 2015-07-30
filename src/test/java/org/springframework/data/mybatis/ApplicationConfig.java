97e70648648abde60a857995e66ffc6e4f393f8f
lewis-ing <liuwenlong62555@126.com>
2015-07-30 14:23:07 +0800

    提供性能,修改配置信息

--- src/test/java/org/springframework/data/mybatis/ApplicationConfig.java
+++ src/test/java/org/springframework/data/mybatis/ApplicationConfig.java
@@ -1,12 +1,10 @@
 package org.springframework.data.mybatis;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mybatis.repository.config.EnableMyBatisRepositories;
 
-@Configuration
 @EnableMyBatisRepositories
-@EnableAutoConfiguration
+@SpringBootApplication
 public class ApplicationConfig {
 
 }
